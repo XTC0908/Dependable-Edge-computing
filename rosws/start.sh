74eb0166d39235c44972682cdebc96cc55862bb0
luxtonychen <luxtonychen@gmail.com>
2018-11-28 17:12:02 +0100

    ksajdfbjKE

--- rosws/start.sh
+++ rosws/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -rf ~/Dependable-Edge-computing/rosws/devel
-rm -rf ~/Dependable-Edge-computing/rosws/build
-cd ~/Dependable-Edge-computing/rosws
-catkin_make
+cd /Dependable-Edge-computing/rosws
+source devel/setup.sh
+roscore > /tmp/core_log.txt 2>&1 &
+rosrun edge_info vhc1.py > /tmp/vhc1_log.txt 2>&1 &
+roslaunch bridge rosbridge_websocket.launch > /tmp/bridge_log.txt 2>&1 &
