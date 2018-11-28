bfcdbf4952f909d94e01501275b3a5111ab1b9c8
luxtonychen <luxtonychen@gmail.com>
2018-11-28 15:18:47 +0100

    start from start.sh

--- rosws/devel/setup.sh
+++ rosws/devel/setup.sh
@@ -10,7 +10,8 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/yulans/Documents/edge/cnedge/Dependable-Edge-computing/rosws/devel}
+#: ${_CATKIN_SETUP_DIR:=/home/yulans/Documents/edge/cnedge/Dependable-Edge-computing/rosws/devel}
+: ${_CATKIN_SETUP_DIR:=./}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
