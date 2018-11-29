c26c00235e290d8e977e3578d1db471b1a4e74b8
luxtonychen <luxtonychen@gmail.com>
2018-11-29 21:06:21 +0100

    docker file

--- rosws/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /Dependable-Edge-computing/rosws
-source devel/setup.sh
-roscore > /tmp/core_log.txt 2>&1 &
-rosrun edge_info vhc1.py > /tmp/vhc1_log.txt 2>&1 &
-roslaunch bridge rosbridge_websocket.launch > /tmp/bridge_log.txt 2>&1 &
