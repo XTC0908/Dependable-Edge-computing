d91353ce6d0df9c2b86af6e96d90d9ae27f265f1
ly-nehs <44581260+ly-nehs@users.noreply.github.com>
2018-12-05 14:18:36 +0100

    new publish and subscribe client

--- rosws/sub_client.py
+++ rosws/sub_client.py
@@ -7,8 +7,8 @@ import socket
 import pprint
 import websocket
 import argparse
-
-
+import threading
+import sys
 #args = parser.parse_args()
 
 print("BSON-ROSBridge Testclient")
@@ -22,38 +22,59 @@ WEBSOCKET_URL = "ws://"+TCP_IP+":"+str(TCP_PORT)
 TEST_MODE = "JSON"
 COMMUNICATION_METHOD = "TCP"
 
-def cmdFunction(vhcid,cmd):
-  vhcid = vhcid
-  cmd_message = {
-    "vhcid" : vhcid,
-    "cmd" : cmd
+def monitorGeopoint():
+  geo_msg_info = {
+    "op": "subscribe",
+    "topic": "/geopoint",
+    "type": "edge_info/vhc_geo"
   }
-  return cmd_message
+  try:
+    ws = websocket.create_connection(WEBSOCKET_URL)
+    print("The connection is successful!")
+  except:
+    print("The connection fails!")
+  geo_info_json = json.dumps(geo_msg_info)
+  ws.send(geo_info_json)
+  while(1):
+    rcv_json = ws.recv()
+    type_msg = json.loads(rcv_json)['op']
+    if type_msg == 'publish':
+      msgs_conf = json.loads(rcv_json)
+      dictionary = json.loads(rcv_json)['msg']['geo']['latitude']
+      print(dictionary)
+
+def monitorObs():
+  obs_msg_info = {
+    "op": "subscribe",
+    "topic": "/obspoint",
+    "type": "edge_info/vhc_geo"
+  }
+  try:
+    obsws = websocket.create_connection(WEBSOCKET_URL)
+    print("The connection is successful!")
+  except:
+    print("The connection fails!")
+  
+  obs_info_json = json.dumps(obs_msg_info)
+  obsws.send(obs_info_json)
+  while(1):
+    obs_json = obsws.recv()
+    type_msg = json.loads(obs_json)['op']
+    if type_msg == 'publish':
+      msgs_conf = json.loads(obs_json)
+      dictionary = json.loads(obs_json)['msg']['geo']['latitude']
+      print(dictionary)
+
+monitorGeopoint()
+
+T1 = threading.Thread(target = monitorGeopoint,args = ())
+T2 = threading.Thread(target = monitorObs,args = ())
+T1.start()
+T2.start()
 
 
-geo_msg_info = {
-  "op": "subscribe",
-  "topic": "/geopoint",
-  "type": "edge_info/vhc_geo"
-}
 
 
-try:
-  ws = websocket.create_connection(WEBSOCKET_URL)
-  print("The connection is successful!")
-except:
-  print("The connection fails!")
-
-geo_info_json = json.dumps(geo_msg_info)
-ws.send(geo_info_json)
-while(1):
-  rcv_json = ws.recv()
-  type_msg = json.loads(rcv_json)['op']
-  print(type_msg)
-  if type_msg == 'publish':
-    msgs_conf = json.loads(rcv_json)
-    dictionary = json.loads(rcv_json)['msg']['geo']['latitude']
-    print(dictionary)
 
 
 
