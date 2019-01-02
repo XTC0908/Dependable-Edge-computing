63d666f9ac57cded82b0b61fd3b086063089ea80
zekun <zekun_du@163.com>
2019-01-02 06:47:36 -0800

    add documentation -dzk

--- Viz/viz_map.py
+++ Viz/viz_map.py
@@ -1,3 +1,11 @@
+# This file is used to visualize the map.
+# On the map, roads, edges (cars) and their conditions are included.
+# For roads, "red" color represents jammed, while "green" color represents smooth.
+# For edges, a dot of blue color represents a car. Every edge has a respective risk zone.
+# Risk zone is a dash-line rectangle. "red" color represents in the edge is in risk, while "green" color safe.
+# This file includes visualization functions and server building.
+# The system which runs this file is a server to receive data from digital twin which updates object status. 
+
 import tkinter as tk
 import osmnx as ox
 import sys
@@ -13,14 +21,7 @@ from threading import Thread
 import sys
 import os
 
-request = 'can you hear me?'
-car_request = '{"category": "car", "id": 1, "x": 333307.4094043318, "y": 6581667.751225858}'
-safety_zone_request = '{"category": "safetyzone", "id": 1, "x": 333307.4094043318, "y": 6581667.751225858, \
-"dot1": (333320, 6581560), "dot2": (333307, 6581560), "dot3": (333307, 6581670), \
-"dot4": (333320, 6581670), "color": "#162C22"}'
-road_request = '{"category": "road", "ux": 333307.4094043318, "uy":6581667.751225858,\
-"vx": 333294.50284388475, "vy": 6581655.340821373, "state": "jam"}' # "jam" or "smooth"
-
+# Following is the setting of server building
 
 def ConstructHandler(message_queue, root):
     class PostHandler(BaseHTTPRequestHandler):
@@ -47,7 +48,7 @@ def ConstructHandler(message_queue, root):
     
     return PostHandler
 
-def start_server(post_handler, address=('127.0.0.1', 8888)):
+def start_server(post_handler, address=('127.0.0.1', 8888)): # address can be adjusted due to different conditions
     print(os.getppid())
     httpd = HTTPServer(address, post_handler)
     #try:
@@ -58,19 +59,19 @@ def start_server(post_handler, address=('127.0.0.1', 8888)):
     #httpd.server_close()
 
 
+# RoadMap class supports the main object information storage and update.
 class RoadMap(object):
-    
     cars = []
     jam_edge = []
     smooth_edge = []
     safetyzone = []
 
+    # initialization the class
+    # define data framework
     def __init__(self, road_map=None):
         self.jam_edge = []
         self.smooth_edge = []
         self.cars = []
-        #self.cars = [{"id": 1, "x": 333307.4094043318, "y": 6581667.751225858},\
-        #    {"id": 2, "x": 333307, "y": 6581560}]
         self.safetyzone = []
         self.map = road_map
         if road_map != None:
@@ -78,23 +79,9 @@ class RoadMap(object):
             self.bbox = edges.total_bounds #west, south, east, north
         else:
             self.bbox = None
-        
-    #def rotate(self, angle, center):
-    #    angle = math.radians(angle)
-    #    cos_val = math.cos(angle)
-    #    sin_val = math.sin(angle)
-    #    cx, cy = center
-    #    new_points = []
-    #    for x_old, y_old in self.rectangle_points:
-    #        x_old -= cx
-    #        y_old -= cy
-    #        x_new = x_old * cos_val - y_old * sin_val
-    #        y_new = x_old * sin_val + y_old * cos_val
-    #        new_points.append([x_new + cx, y_new + cy])
-    #    self.rotate_rectangle = new_points
 
+    # risk zone drawing function
     def draw_square(self, handler, dot_list, color):
-        #p = self.rotate_rectangle
         p = dot_list
         if color == 'red':
             color = '#E91822'
@@ -105,15 +92,17 @@ class RoadMap(object):
         handler.create_line(p[0][0], p[0][1], p[3][0], p[3][1], width=5, fill=color, dash = 5)
         handler.create_line(p[2][0], p[2][1], p[3][0], p[3][1], width=5, fill=color, dash = 5)
 
+    # draw roads which are jammed
     def draw_jammed_edge(self, handler):
         # red color
         G = self.map
-        mapping = self.__mapping__(self.width, self.height)
+        mapping = self.__mapping__(self.width, self.height) # a simpler form of mapping function, defined below
         for u, v in self.jam_edge:
             x_1, y_1 = mapping(G.nodes[u]['x'], G.nodes[u]['y'])
             x_2, y_2 = mapping(G.nodes[v]['x'], G.nodes[v]['y'])
             handler.create_line(x_1, y_1, x_2, y_2, width=10, fill='#F81818')
 
+    # draw roads which are not jammed
     def draw_smooth_edge(self, handler):
         # green color
         G = self.map
@@ -123,48 +112,41 @@ class RoadMap(object):
             x_2, y_2 = mapping(G.nodes[v]['x'], G.nodes[v]['y'])
             handler.create_line(x_1, y_1, x_2, y_2, width=10, fill='#07B123')
     
+    # store map information
     def load_map(self, path):
         self.map = ox.load_graphml(path)
         edges = ox.graph_to_gdfs(self.map, nodes=False, fill_edge_geometry=True)
         self.bbox = edges.total_bounds #west, south, east, north
 
+    # draw initial map roads with gray color
     def draw_map(self, handler): # handler -> canvas
-        handler.delete('all')
+        handler.delete('all') # clean canvas
         width, height = handler.winfo_width(), handler.winfo_height()
         self.width, self.height = width, height
         G = self.map
         mapping = self.__mapping__(width, height)
-
-        #print(G.edges(keys=False, data=False))
-        #print(G.nodes)
         
         for u, v in G.edges(keys=False, data=False):
-            #print(G.nodes[u]['x'], G.nodes[u]['y'], G.nodes[v]['x'], G.nodes[v]['y'], '\n')
             x_1, y_1 = mapping(G.nodes[u]['x'], G.nodes[u]['y'])
             x_2, y_2 = mapping(G.nodes[v]['x'], G.nodes[v]['y'])
-            handler.create_line(x_1, y_1, x_2, y_2, width=10, fill='#B0B0B0')
+            handler.create_line(x_1, y_1, x_2, y_2, width=10, fill='#B0B0B0') # gray color
             
 
-
+    # update a jammed road information in map storage
     def input_jammed_node(self, ux, uy, vx, vy, color=None):
         # input: a node which is a part of a jammed edge
         # jammed_node: longitude and latitude, similar to G.nodes[u]['x'], G.nodes[u]['y'] in draw_map function
         # the jammed edge is added into self.smooth_edge (coordinates of canvas)
     
-        #G = self.map
         print('####')
         u_red, v_red = ux, vx
-        #print (ux, uy, vx, vy)
-        #for u, v in G.edges(keys=False, data=False):
-        #    if G.nodes[u]['lat'] == ux and G.nodes[u]['lon'] == uy and G.nodes[v]['lat'] == vx and G.nodes[v]['lon'] == vy:
-        #        u_red, v_red = u, v 
-        #        break  
         self.jam_edge.append((u_red, v_red))
         for u, v in self.smooth_edge:
             if u == u_red and v == v_red:
                 self.smooth_edge.remove((u, v))
 
         
+    # update a non-jammed road information in map storage
     def input_smooth_node(self, ux, uy, vx, vy):
         # input: a node which is a part of a jammed edge
         # jammed_node: longitude and latitude, similar to G.nodes[u]['x'], G.nodes[u]['y'] in draw_map function
@@ -181,7 +163,7 @@ class RoadMap(object):
             if u == u_red and v == v_red:
                 self.jam_edge.remove((u, v))
 
-        
+    # update (add) car information in the map storage
     def input_cars(self, car_dic_element):
         # car_dic_element is a dic type element
         print('\n\nbefore input car  ', self.cars)
@@ -191,23 +173,24 @@ class RoadMap(object):
         newcar = {'id': ID, 'x': x, 'y': y}
         for c in self.cars:
             if c['id'] == ID:
-                self.cars.remove(c)
+                self.cars.remove(c) # check and remove old and redundant car information
                 break
         self.cars.append(newcar)    
-        print('\nafter input car  ', self.cars)
+        print('\nafter input car  ', self.cars) # showing current car information
 
+    # draw cars on the canvas
     def draw_cars(self, handler):
         # car_element is an element of self.car
         # draw cars
         G = self.map
         mapping = self.__mapping__(self.width, self.height)
-
         for c in self.cars:
             x, y = c['x'], c['y']
             x_1, y_1 = mapping(x, y)
             handler.create_oval(x_1 - 10, y_1 - 10, x_1 + 10, y_1 + 10, fill="#07C8C5")
             #light blue dot represent car
 
+    # update safety zone information in the storage
     def input_safetyzone(self, sz_element):
         print('\n\nbefore input sftyz: ', self.safetyzone)
         mapping = self.__mapping__(self.width, self.height)
@@ -224,24 +207,26 @@ class RoadMap(object):
         sz = {'id': ID, 'dots': dots, 'color': color}
         for s in self.safetyzone:
             if s['id'] == ID:
-                self.safetyzone.remove(s)
+                self.safetyzone.remove(s) # check and delete old and redundant safety zone
                 break
         self.safetyzone.append(sz)
         print('\nafter input sftyz: ', self.safetyzone)
-
+    
+    # draw all safety zones 
     def draw_safetyzone(self, handler):
         for sz in self.safetyzone:
             dot = sz['dots']
             if sz['color'] == 'red':
-                color = '#C70039'
+                color = '#C70039' # red
             else:
-                color = '#29A71A'
+                color = '#29A71A' # green
             print(dot)
             handler.create_line(dot[0][0], dot[0][1], dot[1][0], dot[1][1], width=8, fill=color, dash = 3)
             handler.create_line(dot[2][0], dot[2][1], dot[1][0], dot[1][1], width=8, fill=color, dash = 3)
             handler.create_line(dot[0][0], dot[0][1], dot[3][0], dot[3][1], width=8, fill=color, dash = 3)
             handler.create_line(dot[2][0], dot[2][1], dot[3][0], dot[3][1], width=8, fill=color, dash = 3)
 
+    # define a mapping function
     def __mapping__(self, w, h):
         offset_x, offset_y = self.bbox[0], self.bbox[3]
         scale_x = w/(self.bbox[2] - self.bbox[0])
@@ -249,47 +234,24 @@ class RoadMap(object):
 
         return lambda x, y: (int((x-offset_x)*scale_x), int((y-offset_y)*scale_y))
 
-
+# initial canvas info
 def init_canvas(master, width, height):
     canvas = tk.Canvas(master, width=width, height=height, bg='white')
     canvas.pack()
     master.update_idletasks()
     return canvas
 
-#def receive_data(road_map, s, handler):
-#
-#    # accept and establish connection
-#    conn, addr = s.accept()
-#    # receive message
-#    request    = conn.recv(1024).decode('utf-8')
-# 
-#    print('request is: ',request)
-#    print('Connected by', addr)
-#    # send message
-#    reply = 'Yes : '
-#    conn.sendall(reply.encode('utf-8'))
-#    
-#    #recv_dic = json.loads(request)
-#    recv_dic = ast.literal_eval(request)
-#    if recv_dic['category'] == 'safetyzone':
-#        print(recv_dic['id'])
-#
-#    #testing
-#    G = road_map.map
-#    mapping = road_map.__mapping__(road_map.width, road_map.height)
-#    
-#    for x, y in road_map.cars:
-#        x_1, y_1 = mapping(x, y)
-#        handler.create_oval(x_1 - 10, y_1 - 10, x_1 + 10, y_1 + 10, fill='#FFFFBF')
-#    #end of testing
-#    root.after(3500, receive_data, road_map, s, handler)
-
+# exit function
 def close_window(event, server):
     server.join(timeout=0.1)
     sys.exit()
 
+# receive message and update storage and refresh the canvas
 def update_window(event, message, road_map, handler):
     try:
+        # receive a new massage
+        # check if it is legal
+        # if legal, do respective command
         message_body = message.get(timeout=1)
         if message_body["category"] == 'car':
             road_map.input_cars(message_body)
@@ -320,7 +282,7 @@ def update_window(event, message, road_map, handler):
     road_map.draw_cars(handler)
     road_map.draw_safetyzone(handler)
 
-
+# main function to initialize the system and run it.
 if __name__ == "__main__":
     print(os.getppid())
 
